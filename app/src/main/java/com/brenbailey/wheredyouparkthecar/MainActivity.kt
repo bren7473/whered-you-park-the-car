ec7281cf9e73d41439cb4a2a039ff266a30d923f
Bren Bailey <bren7473@gmail.com>
2021-10-13 13:15:19 -0400

    initial multiple branch push

--- app/src/main/java/com/brenbailey/wheredyouparkthecar/MainActivity.kt
+++ app/src/main/java/com/brenbailey/wheredyouparkthecar/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.brenbailey.wheredyouparkthecar
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.brenbailey.wheredyouparkthecar.databinding.MapFragmentBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
--- app/src/main/java/com/brenbailey/wheredyouparkthecar/ui/maps/MapFragment.kt
+++ app/src/main/java/com/brenbailey/wheredyouparkthecar/ui/maps/MapFragment.kt
@@ -1,5 +1,7 @@
 package com.brenbailey.wheredyouparkthecar.ui.maps
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
@@ -7,7 +9,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.brenbailey.wheredyouparkthecar.R
 import com.brenbailey.wheredyouparkthecar.databinding.MapFragmentBinding
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -15,12 +18,13 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MapFragment : Fragment() {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var layout: View
     private var mapReady = false
     private lateinit var carLocation: LatLng
     private var binding: MapFragmentBinding? = null
@@ -36,8 +40,6 @@ class MapFragment : Fragment() {
     }
 
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -51,28 +53,105 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        layout = view
         view.let {
-            viewModel.carLocation.observe(viewLifecycleOwner, {
-                    carLocation -> this.carLocation = carLocation
+            viewModel.carLocation.observe(viewLifecycleOwner, { carLocation ->
+                this.carLocation = carLocation
                 updateMap()
             })
 
-        val mapFragment =
-            childFragmentManager.findFragmentById(R.id.mapFragmentId) as? SupportMapFragment?
-        mapFragment?.getMapAsync { googleMap ->
-            mMap = googleMap
-            mapReady = true
-            //updateMap()
-            val sydney = LatLng(-34.0, 151.0)
-            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        }
-    }
+            val mapFragment =
+                childFragmentManager.findFragmentById(R.id.mapFragmentId) as? SupportMapFragment?
+            mapFragment?.getMapAsync { googleMap ->
+                mMap = googleMap
+                mapReady = true
+                //updateMap()
+                val sydney = LatLng(-34.0, 151.0)
+                mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-}
+                checkPermissions()
+            }
+        }
+
+    }
 
     private fun updateMap() {
-        if (mapReady && carLocation != null) {
+        TODO("Not yet implemented")
+    }
+
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied")
+            }
+        }
+
+    private fun checkPermissions() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED  &&
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                layout.showDialog(
+                    getString(R.string.permission_granted),
+                    null
+                ) {}
+            }
+            shouldShowRequestPermissionRationale(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                layout.showDialog(
+                    getString(R.string.permission_required),
+                    getString(R.string.ok)
+                ) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                }
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
         }
     }
+
+    fun View.showDialog(
+        msg: String,
+        actionMessage: CharSequence?,
+        action: (View) -> Unit
+    ) {
+
+        if (actionMessage != null) {
+            MaterialAlertDialogBuilder(context,
+                R.style.ThemeOverlay_MaterialComponents_Dialog_Alert)
+                .setMessage(msg)
+                .setPositiveButton(actionMessage) { dialog, which ->
+                    action(this)
+                }
+                .show()
+        } else {
+            MaterialAlertDialogBuilder(context,
+                R.style.ThemeOverlay_MaterialComponents_Dialog_Alert)
+                .setMessage(msg)
+                .setPositiveButton(resources.getString(R.string.ok)) { dialog, which ->
+                    // Respond to positive button press
+                }
+                .show()
+        }
+    }
+
 }
