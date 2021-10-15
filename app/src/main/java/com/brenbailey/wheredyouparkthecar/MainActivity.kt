5094733d3262c1a2b339105698ed1a1a15fca25e
Bren Bailey <bren7473@gmail.com>
2021-10-15 14:29:32 -0400

    Material Tool Bar implemented with button
    Button needs functionality
    Need to convert viewmodel to sharedViewModel in order
    to have toolbar button press affect data for map fragment

--- app/src/main/java/com/brenbailey/wheredyouparkthecar/MainActivity.kt
+++ app/src/main/java/com/brenbailey/wheredyouparkthecar/MainActivity.kt
@@ -1,4 +1,25 @@
 package com.brenbailey.wheredyouparkthecar
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+
+    private lateinit var topAppBar: MaterialToolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        topAppBar = findViewById(R.id.topAppBar)
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.favorite -> {
+                    Log.d("distance", "clicked")
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
